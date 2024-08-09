68738afa0b16c7e8f44484da46aa08e016590b7c
Birendra <bibekbhusal400@gmail.com>
2024-08-09 21:38:00 +0545

    make navbar responsive

--- frontend1/src/components/Navbar.jsx
+++ frontend1/src/components/Navbar.jsx
@@ -5,10 +5,20 @@ import { LuCalendarDays } from "react-icons/lu";
 import { HiOutlineBellAlert } from "react-icons/hi2";
 import { CiSearch } from "react-icons/ci";
 import TodoContext from "../Context/TodoContext";
-
+import { RxHamburgerMenu } from "react-icons/rx";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { RxCross1 } from "react-icons/rx";
 const Navbar = () => {
   const TodoItems = useContext(TodoContext);
 
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const handleShowCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
+  const [value, onChange] = useState(new Date());
   const [search, setSearch] = useState("");
 
   const handleSearch = (e) => {
@@ -18,7 +28,14 @@ const Navbar = () => {
   const filterData = TodoItems.filter((item) =>
     item.todoTitle.toLowerCase().includes(search.toLowerCase())
   );
-  console.log("filterData", filterData);
+  const [showNavBar, setShowNavBar] = useState(false);
+  const [closeNavbar, setCloseNavbar] = useState(true);
+
+  const handleMobileResponsivnese = () => {
+    setShowNavBar(!showNavBar);
+    setCloseNavbar(!closeNavbar);
+  };
+
   return (
     <>
       <div
@@ -27,37 +44,70 @@ const Navbar = () => {
       >
         {/* fixed top-0 left-0  */}
         <div
-          className="grid grid-cols-3 justify-between  w-[90%] h-full items-center
+          className="  w-[90%] h-full relative grid grid-cols-3 justify-between  items-center 
     "
         >
-          {" "}
-          <Link to="/" className=" no-underline">
-            <div className="flex text-black mb-0 ">
-              <h5 className="text-[24px] font-semibold">To-</h5>
-              <h5 className="text-red-400 mb-0 text-[24px] font-semibold">
-                Do
-              </h5>
-            </div>
-          </Link>
-          <div className="flex justify-start items-center">
-            <input
-              type="search"
-              name=""
-              id=""
-              value={search}
-              onChange={handleSearch}
-              className="w-[400px] p-[6px] outline-none text-black"
-            />
-            <button className="bg-red-400 px-[6px] py-[6px] rounded-sm">
-              <CiSearch className=" size-6 text-white" />
-            </button>
+          <div className="grid grid-cols-3 justify-between  items-center w-full md:col-span-1  lg:col-span-1 col-span-3 relative">
+            {" "}
+            <Link
+              to="/"
+              className=" no-underline col-span-2 md:col-span-1 lg:col-span-1"
+            >
+              <div className="flex text-black mb-0 ">
+                <h5 className="text-[24px] font-semibold mb-0">To-</h5>
+                <h5 className="text-red-400  text-[24px] font-semibold mb-0">
+                  Do
+                </h5>
+              </div>
+            </Link>{" "}
+            <span
+              className="text-black  lg:hidden md:hidden flex justify-end col-span-1"
+              onClick={handleMobileResponsivnese}
+            >
+              <RxHamburgerMenu size={24} />
+            </span>
           </div>
-          <div className="flex  items-center justify-center gap-4">
-            <button className="bg-red-400 p-1 rounded-xl">
-              <HiOutlineBellAlert className="size-6 text-white" />{" "}
-            </button>
-            <div className="bg-red-400 p-1 rounded-xl">
-              <LuCalendarDays className=" size-6 text-white" />
+          <div
+            className={`md:grid md:grid-cols-2 md:col-span-2  p-2 w-auto 
+           md:p-0 lg:p-0  ${
+             showNavBar
+               ? "show bg-[#00000073]  w-full col-span-3 left-[-19px] absolute top-0 h-[100vh] flex flex-col gap-5"
+               : "hidden"
+           }`}
+          >
+            <span
+              className="text-black  lg:hidden md:hidden flex justify-end "
+              onClick={handleMobileResponsivnese}
+            >
+              <RxCross1 />
+            </span>
+            <div className="flex justify-start items-center">
+              <input
+                type="search"
+                name=""
+                id=""
+                value={search}
+                onChange={handleSearch}
+                className="w-[280px]  md:w-[400px] lg:w-[400px] p-[6px] outline-none text-black"
+              />
+              <button className="bg-red-400 px-[6px] py-[6px] rounded-sm">
+                <CiSearch className=" size-6 text-white" />
+              </button>
+            </div>
+            <div className="flex  items-center justify-around flex-col md:flex-row lg:flex-row">
+              <button className="bg-red-400 p-1 rounded-xl">
+                <HiOutlineBellAlert className="size-6 text-white" />{" "}
+              </button>
+              <div
+                className="bg-red-400 p-1 rounded-xl relative"
+                onClick={handleShowCalendar}
+              >
+                <LuCalendarDays className=" size-6 text-white" />
+              </div>
+              <span className="text-gray-500 font-medium">
+                {" "}
+                {value.toDateString()}
+              </span>{" "}
             </div>
           </div>
         </div>{" "}
@@ -81,6 +131,13 @@ const Navbar = () => {
             )}
           </span>
         </div>
+      )}
+      {showCalendar && (
+        <Calendar
+          onChange={onChange}
+          value={value}
+          className="absolute right-20 top-24"
+        />
       )}
     </>
   );
